3b3f726fae9ca9223f7937fe8550f159018d46a2
Onur Alkan <onuralken@gmail.com>
2022-09-02 03:07:59 +0300

    fix error handler

--- app/core/middlewares.ts
+++ app/core/middlewares.ts
@@ -1,6 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-export const errorHandler = (err: Error, req: Request, res: Response) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   console.error(err.stack);
   res.status(500).send(err.message);
 };
